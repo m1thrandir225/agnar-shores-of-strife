cbd644623e87ea1cfef849e1fa76da1eaa3d2e27
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-09-11 23:35:42 +0200

    fix: main.gd updates

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,26 +4,26 @@ extends Node2D
 @onready var camera: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-    if player:
-        player.health_changed.connect(on_player_health_changed)
-        player.player_died.connect(_on_player_died)
-        player.player_respawned.connect(_on_player_respawned)
+	if player:
+		player.health_changed.connect(on_player_health_changed)
+		player.player_died.connect(_on_player_died)
+		player.player_respawned.connect(_on_player_respawned)
 
 func setup_camera_limits() -> void:
-    if camera:
-        camera.limit_left = 0
-        camera.limit_top = 0
-        camera.limit_right = 0
-        camera.limit_bottom = 0
+	if camera:
+		camera.limit_left = 0
+		camera.limit_top = 0
+		camera.limit_right = 0
+		camera.limit_bottom = 0
 
 func on_player_health_changed(new_health: int) -> void:
-    print("Player health changed: ", new_health)
+	print("Player health changed: ", new_health)
 
 func _on_player_died() -> void:
-    print("Player died")
+	print("Player died")
 
-    await get_tree().create_timer(3.0).timeout
-    get_tree().reload_current_scene()
+	await get_tree().create_timer(3.0).timeout
+	get_tree().reload_current_scene()
 
 func _on_player_respawned() -> void:
-    print("Player respawned")
+	print("Player respawned")
